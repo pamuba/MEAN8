4f593ff1d64fc674251f37ca746345560733187a
pamuba <pamuba@users.noreply.github.com>
2020-07-07 23:21:47 +0530

    21st

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Video = require('../models/video');
 
-router.get('/', function(req, res){
-    res.send('app works')
+const db = "mongodb://localhost:27017/videoLibrary";
+mongoose.Promise = global.Promise;
+
+//Connect
+mongoose.connect(db, function(err){
+    if(err){
+        console.error("Error!"+err)
+    }
+    else{
+        console.log("Mongodb Connected")
+    }
+});
+
+
+router.get('/videos', function(req, res){
+    console.log("Get Request for all videos");
+
+    Video.find({})
+         .exec(function(err, videos){
+             if(err){
+                 console.log("Error retrieving videos");
+             }
+             else{
+                 res.json(videos);
+             }
+         });
 });
 
 module.exports = router;
