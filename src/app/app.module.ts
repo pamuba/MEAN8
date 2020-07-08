ba03f7da06a51cb1901ebfc9fb5c107faac32b4b
pamuba <pamuba@users.noreply.github.com>
2020-07-08 23:10:43 +0530

    31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { VideoCenterComponent } from './video-center/video-center.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { SafePipe } from './safe.pipe'
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http'
     VideoCenterComponent,
     VideoListComponent,
     VideoDetailComponent,
+    SafePipe,
   ],
   imports: [
     BrowserModule,
--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -9,14 +9,7 @@ import { VideoService } from './../video.service'
 })
 export class VideoCenterComponent implements OnInit {
 
-  videos:any=[];
-
-  // videos:Video[] = [
-  //   // {"_id":"1", "title":"Title 1", "url":"url 1", "description":"description 1"},
-  //   // {"_id":"2", "title":"Title 2", "url":"url 2", "description":"description 2"},
-  //   // {"_id":"3", "title":"Title 3", "url":"url 3", "description":"description 3"},
-  //   // {"_id":"4", "title":"Title 4", "url":"url 4", "description":"description 4"}
-  // ]
+  videos:any=[]
 
   selectedVideo:Video;
 
